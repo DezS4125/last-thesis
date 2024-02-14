f904f787b58b89372d46420ba4d1fe26b504470a
dezs <workingdezs@gmail.com>
2024-02-14 12:39:28 +0700

    updated FlaskKafkaBridge

--- webserverDocker/FlaskKafkaBridge.py
+++ webserverDocker/FlaskKafkaBridge.py
@@ -1,13 +1,20 @@
+import os
 from flask import Flask, request
 from confluent_kafka import Producer
 
-p = Producer({'bootstrap.servers': 'localhost:9092'})
+# Read bootstrap servers from environment variables
+bootstrap_servers = os.getenv('BOOTSTRAP_SERVERS')
+
+# Initialize Kafka producer
+p = Producer({'bootstrap.servers': bootstrap_servers})
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_frame():
     frame_data = request.data
     p.produce('webcam_topic', frame_data)
+
     return '', 204
 
 if __name__ == '__main__':
