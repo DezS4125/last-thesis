c14d9210180b545788866685817b1a2f493f128f
dezs <workingdezs@gmail.com>
2024-03-16 10:34:30 +0700

    moved consumer into setup function

--- dataProcessor/processorConsumer.py
+++ dataProcessor/processorConsumer.py
@@ -5,27 +5,26 @@ import json
 topics = ['video-stream-1', 'video-stream-2', 'video-stream-3']
 # topics = ['video-stream-1']
 
-# Set up the Kafka consumer
-consumer = KafkaConsumer(*topics, bootstrap_servers='192.168.1.241:9092')
-
-def setup_producer():
+def setup_consumer_producer():
     try:
+        consumer = KafkaConsumer(*topics, bootstrap_servers='192.168.1.241:9092')
         producer = KafkaProducer(bootstrap_servers="192.168.1.241:9092",value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        return producer
+        return consumer, producer
     except Exception as e:
         if e == 'NoBrokersAvailable':
             print('waiting for brokers to become available')
-        return 'not-ready'
+        return 'not-ready','not-ready'
 
 print('setting up producer, checking if brokers are available')
+consumer='not-ready'
 producer='not-ready'
 
 while producer == 'not-ready':
     print('brokers not available yet')
     time.sleep(5)
-    producer = setup_producer()
+    consumer, producer = setup_consumer_producer()
 
-print('brokers are available and ready to produce messages')
+print('brokers are available and ready to consume and produce messages')
 
 for message in consumer:
     print(f'Received message from topic: {message.topic}')
